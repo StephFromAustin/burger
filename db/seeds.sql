6ad7a26f0b5c8f3529bb2789b27e2933f30ac289
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-09 21:32:13 -0600

    queries inserted into burger table with three entries

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- INSERT QUERIES TO POPUALTE THE BURGERS TABLE WITH AT LEAST THREE ENTIRES 
+
+INSERT INTO burgers (burger_name)
+VALUES ("Do Brussel Burger"), ("Summer Thyme Burger"), ("Peas and Thank You Burger");
