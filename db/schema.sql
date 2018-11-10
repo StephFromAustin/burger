049ce0ac4c6ba8f7974f0d3bdad79d3bff7fdefa
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-09 22:11:10 -0600

    mysql data added to schema.sql file

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE THAT HOLDS BURGER INFORMATION 
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INTEGER (10)
+    AUTO_INCREMENT, 
+    burger_name VARCHAR
+    (100) NOT NULL, 
+    devoured BOOLEAN DAFAULT false NOT NULL, 
+    PRIMARY KEY
+    (id)
+);
