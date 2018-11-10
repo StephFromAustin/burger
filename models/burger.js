620fa952b745f4073bec00190e88476c3128448a
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-10 00:25:07 -0600

    tests

--- models/burger.js
+++ models/burger.js
@@ -20,5 +20,17 @@ let burger = {
     }
 }
 
+burger.all((res) => {
+        console.log(res);
+    });
+    
+    burger.create(['The Googe Burger'], (res) => {
+        console.log(res);
+    });
+    
+    burger.update('id = 1', (res) => {
+        console.log(res);
+    }); 
+
 // EXPORT ROUTER 
 module.exports = burger;
