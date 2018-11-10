ef7b054e279c7ccda4fa68d1f9105de5f559201a
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-09 23:43:33 -0600

    orm.selectAll code

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,12 @@
 // IMPORT THE ORM TO CREATE FUCNTIONS THAT WILL INTERACT WITH THE DB
 const orm = require('../config/orm.js') // DEPENDENCIES
+
+// CONSTRUCTOR THAT WILL CALL ORM FUNCTIONS USING BURGER SPECIFIC INPUT FROM THE ORM
+let burger = {
+    all: (cb) => {
+        orm.selectAll("burgers", (res) => {
+            cb(res);          
+        });
+    },
+    
+}
