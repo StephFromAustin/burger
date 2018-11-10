7b9150ca1f37c675c73b35512381fb4524eec26c
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-09 22:57:56 -0600

    insert new item into table code

--- config/orm.js
+++ config/orm.js
@@ -2,6 +2,7 @@
 const connection = require("../config/connection.js");
 
 // ORM CODE => = function
+    // selectALL = SELECTS ALL DATA FROM TABLE 
 const orm = {
     selectAll: (tableInput, cb) => {
     let queryString = 'SELECT * FROM' + tableInput + ';';
@@ -11,6 +12,21 @@ const orm = {
         }
         cb (result);
     })
+    },
+    // insertOne = INSERT NEW ITEM INTO TABLE 
+    insertOne: (table, columnName, values, cb) => {
+        let queryString = 'INSERT INTO' + table;
+        queryString += ' (' +columnName + ') ';
+        queryString += 'VALUES ( '; 
+        queryString +=  ') '
+        console.log(queryString);
+        connection.query(queryString, values, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            console.log(values)
+            cb(result);
+        });
     },
     
 }
