d51aef94b42f2b2f3464fbfe367080f3830c6fa6
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-09 22:39:01 -0600

    selectAll code created

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,16 @@
 // DEPENDENCIES 
 const connection = require("../config/connection.js");
 
+// ORM CODE => = function
+const orm = {
+    selectAll: (tableInput, cb) => {
+    let queryString = 'SELECT * FROM' + tableInput + ';';
+    connection.query(queryString, (err,result) => {
+        if (err) {
+            throw err;
+        }
+        cb (result);
+    })
+    },
+    
+}
