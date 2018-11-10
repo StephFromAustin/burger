7ecc842fdfa5656ff187607db080825528afe902
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-09 21:44:20 -0600

    finsh dependencies codes

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 // DEPENDENCIES 
+const express = require("express"); // express 
+const PORT = process.env.PORT || 8080; // port
+const app = express();
 
-const express = require("express");
+
+
+
