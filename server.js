7f651c9238ab78c8ff729a6c6a030daf113f5955
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-09 21:48:12 -0600

    static content code

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express"); // express
 const PORT = process.env.PORT || 8080; // port
 const app = express();
 
-
+// STATIC CONTENT FROM THE PUBLIC DIRECTORY 
+app.use(express.static("public"));
 
 
