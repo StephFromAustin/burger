350613d4bc6387ae0b7093653711df8aa7b6fcc2
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-15 15:47:56 -0600

    conenction fix

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
 // MySQL CONNECTION SET UP 
-const mysql = require("mysql"); // DEPENDENCIES 
+const mysql = require("mysql"); //
+const conenction;
+//DEPENDENCIES 
 
 // CONNECTION SET UP 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+if (process.env.JWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+;} else {
     connection = mysql.createConnection({
-        host: "pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        username: "slxvmka0x3466jqa",
-        password: "Steph2845",
-        port:"3306",
-        user: "root",
-        // password: "Steph2845",
-        database: "	q44qv46dzdqhfv88"
+        host:'localhost',
+        user:'root',
+        password:'Steph2845',
+        database:'burgers_db',
     });
 };
+
 
 //CONNECTION 
 connection.connect(function(err) {
